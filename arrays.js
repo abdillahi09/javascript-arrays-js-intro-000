9b761a4e30fd2c74347c2a6e0f433086a1ce8ffb
abdillahi Bashir <abdillahi.muse@hotmail.com>
2018-06-22 06:28:04 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,9 +1,43 @@
-chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+function chocolateBars() {
+  var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
+}
 
-addElementToBeginningOfArray = ["Protein Bar", ...chocolateBars];
+function addElementToBeginningOfArray(array, element) {
+  return [element, ...array]
+}
 
-destructivelyAddElementToBeginningOfArray = ["Sugar Bar", ...addElementToBeginningOfArray];
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  array.unshift(element)
+  return array
+}
 
-function accessElementInArray(arg) {
-  return arg + arg[];
-};
+function addElementToEndOfArray(array, element) {
+  return [...array, element]
+}
+
+function destructivelyAddElementToEndOfArray(array, element) {
+  array.push(element)
+  return array
+}
+
+function accessElementInArray(array, index) {
+  return array[index]
+}
+
+function destructivelyRemoveElementFromEndOfArray(array) {
+  array.pop()
+  return array
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(array) {
+  array.shift()
+  return array
+}
+
+function removeElementFromBeginningOfArray(array) {
+  return array.slice(1)
+}
+
+function removeElementFromEndOfArray(array) {
+  return array.slice(0,(array.length-1))
+}
